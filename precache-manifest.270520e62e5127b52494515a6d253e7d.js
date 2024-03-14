fc4fbd302174d9a578cc3e69a0910b5f72cfde4f
Savano Miatama <vano_202228@yahoo.com>
2024-03-14 22:16:36 +0700

    deploy

---
+++ precache-manifest.270520e62e5127b52494515a6d253e7d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe72de924f98da43a1f",
+    "url": "/css/about.04d501c0.css"
+  },
+  {
+    "revision": "d00284b0260585a475de",
+    "url": "/css/app.180dc407.css"
+  },
+  {
+    "revision": "21f0019ad6f96d75ddc9",
+    "url": "/css/chunk-vendors.34f8b12f.css"
+  },
+  {
+    "revision": "7ac8a54211da9828194f23f393b7e40d",
+    "url": "/css/feathericon.css"
+  },
+  {
+    "revision": "952b8d024a5c0730c6e88a5c70927be7",
+    "url": "/css/feathericon.min.css"
+  },
+  {
+    "revision": "2ba34db7fb1c319e91ba098c4250aab2",
+    "url": "/fonts/feathericon.eot"
+  },
+  {
+    "revision": "77b4a05a6511619bf327a89735b0a5b7",
+    "url": "/fonts/feathericon.svg"
+  },
+  {
+    "revision": "2da0719c5095d32f9e3f284ffed19a15",
+    "url": "/fonts/feathericon.ttf"
+  },
+  {
+    "revision": "6b36f567c009df0a476d61999d381d2f",
+    "url": "/fonts/feathericon.woff"
+  },
+  {
+    "revision": "d3f2857699e18bb6431819f2d5eb1619",
+    "url": "/fonts/feathericon.woff2"
+  },
+  {
+    "revision": "2491ce981d2a95771b61ffbef687de79",
+    "url": "/img/21.2491ce98.jpg"
+  },
+  {
+    "revision": "d5e4f92aa054d4bd35c1b3ecc4776959",
+    "url": "/img/ali.d5e4f92a.png"
+  },
+  {
+    "revision": "b6917ea60c466620ac9e170e9b6b203b",
+    "url": "/img/avatar.b6917ea6.png"
+  },
+  {
+    "revision": "e3b86b0b76e5896608b5cb196a1ed2ef",
+    "url": "/img/be.e3b86b0b.jpg"
+  },
+  {
+    "revision": "988812854386c8ed053d2cfc11594e85",
+    "url": "/img/blank.98881285.jpg"
+  },
+  {
+    "revision": "b5b95956b7642928d7fd6efa98c9def2",
+    "url": "/img/book.b5b95956.jpg"
+  },
+  {
+    "revision": "e85afce61c891dd529bb4475422da4c6",
+    "url": "/img/fe.e85afce6.jpg"
+  },
+  {
+    "revision": "b0f622db66cab5b37b42c6c4cd7d0d76",
+    "url": "/img/gakindo.b0f622db.png"
+  },
+  {
+    "revision": "9cf54beaad76d24a59c1548dbe9064d8",
+    "url": "/img/galeri-batik.9cf54bea.jpg"
+  },
+  {
+    "revision": "bba7167a25fe5f4c25f76a456f9472c6",
+    "url": "/img/lib.bba7167a.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c9d35671e641f2a6c7e83635b5cf9677",
+    "url": "/img/manage.c9d35671.png"
+  },
+  {
+    "revision": "25806f94ccb7a9ea711264673d4a955e",
+    "url": "/img/mnt.25806f94.jpg"
+  },
+  {
+    "revision": "0783849967129a4ad1792610cd20eb1b",
+    "url": "/img/mobile.07838499.jpg"
+  },
+  {
+    "revision": "448f95275afdb48c5e20c3d8ee193464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe72de924f98da43a1f",
+    "url": "/js/about.d7856bb2.js"
+  },
+  {
+    "revision": "d00284b0260585a475de",
+    "url": "/js/app.e7866e2e.js"
+  },
+  {
+    "revision": "21f0019ad6f96d75ddc9",
+    "url": "/js/chunk-vendors.c792735c.js"
+  },
+  {
+    "revision": "7d3c9bc856249129b685c3747ba0d5af",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
